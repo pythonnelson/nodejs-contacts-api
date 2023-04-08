b076d11885378358bac9843cbd272e700f78c34e
pythonnelson <isaacnsbkargbo20@gmail.com>
2023-04-08 22:46:38 +0000

    Update user ref to contact

--- backend/models/contactModel.js
+++ backend/models/contactModel.js
@@ -3,11 +3,11 @@ const mongoose = require("mongoose");
 const contactSchema = mongoose.Schema({
     last_name: {
         type: String,
-        required: [true, "Last name is required"],
+        required: [true, "Last name field is required"],
     },
     first_name: {
         type: String,
-        required: [true, "First name is required"],
+        required: [true, "First name field is required"],
     },
     others: {
         type: String,
@@ -15,12 +15,28 @@ const contactSchema = mongoose.Schema({
     },
     email: {
         type: String,
-        required: [true, "Email is required"],
+        required: [true, "Email field is required"],
     },
     phone: {
         type: String,
-        required: [true, "Phone is required"],
-    }
+        required: [true, "Phone field is required"],
+    },
+    address: {
+        type: String,
+        required: [true, "Address field is required"],
+    },
+    country: {
+        type: String,
+        required: [true, "Country field is required"],
+    },
+    state: {
+        type: String,
+        required: [true, "State field is required"],
+    },
+    isActive: {
+        type: Boolean,
+        default: true,
+    },
 }, {
     timestamps: true
 });
---
+++ backend/models/userModel.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+    user_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "User",
+    },
+    full_name: {
+        type: String,
+        required: [true, "Fullname is required"],
+    },
+    username: {
+        type: String,
+        required: [true, "Username is required"],
+        unique: [true, "Username already exists"],
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: [true, "Email already exists"],
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+    },
+}, {
+    timestamps: true,
+});
+
+module.exports = mongoose.model("User", userSchema);
