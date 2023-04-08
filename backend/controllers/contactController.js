276b2264afabd9f97425c483395f5cf2399b87d9
pythonnelson <isaacnsbkargbo20@gmail.com>
2023-04-08 18:43:16 +0000

    Established mongodb connection

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -1,37 +1,47 @@
+const asyncHandler = require('express-async-handler');
+
 // Get all contacts
 // Route: GET /api/v1/contacts
 // Access: Public (for now)
-const getContacts = (req,res) => {
+const getContacts = asyncHandler(async (req,res) => {
     res.status(200).json({ message: "All Contacts" });
-};
+});
 
 // Create contact
 // Route: GET /api/v1/contacts
 // Access: Public (for now)
-const viewContact = (req,res) => {
+const viewContact = asyncHandler(async (req,res) => {
     res.status(200).json({ message: "View Contact" });
-};
+});
 
 // Create contact
 // Route: POST /api/v1/contacts
 // Access: Public (for now)
-const createContact = (req,res) => {
+const createContact = asyncHandler(async (req,res) => {
+    console.log(req.body);
+
+    const { last_name, middle_name, first_name, email, phone, address } = req.body;
+
+    if (!last_name || !first_name || !email || !phone || !address) {
+        res.status(400);
+        throw new Error("Provide details for required fields");
+    }
     res.status(201).json({ message: "Create Contact" });
-};
+});
 
 // Create contact
 // Route: PUT /api/v1/contacts
 // Access: Public (for now)
-const updateContact = (req,res) => {
+const updateContact = asyncHandler(async (req,res) => {
     res.status(200).json({ message: "Update Contact" });
-};
+});
 
 // Create contact
 // Route: DELETE /api/v1/contacts
 // Access: Public (for now)
-const deleteContact = (req,res) => {
+const deleteContact = asyncHandler(async (req,res) => {
     res.status(200).json({ message: "Delete Contact" });
-};
+});
 
 
 
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const errorhandler = require("./middleware/errorhandler");
 const dotenv = require("dotenv").config();
+const connectDb = require("./config/dbConnection");
+
+// Call the DB connection
+connectDb();
+
 const app = express();
 const port = process.env.PORT || 9500;
 
+app.use(express.json());
 app.use("/api/v1/contacts", require("./routes/contactRoutes"));
+app.use(errorhandler);
 
 app.listen(port, () => {
     console.log(`Server running on port: ${port}`);
