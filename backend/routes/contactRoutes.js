9225907dafc77ab74dc5e67c8ca54f2bfaa7b6e1
pythonnelson <isaacnsbkargbo20@gmail.com>
2023-04-08 22:52:05 +0000

    Validate all contact routes

--- backend/routes/contactRoutes.js
+++ backend/routes/contactRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { getContacts, createContact, viewContact,updateContact, deleteContact } = require("../controllers/contactController");
+const validateToken = require("../middleware/validateTokenHandler");
 
-
+router.use(validateToken); // Validate all routes to be protected
 router.route("/").get(getContacts).post(createContact);
 router.route("/:id").get(viewContact).put(updateContact).delete(deleteContact);
 
